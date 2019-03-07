3694b81cdbec32d95e38e0fd9456eb1638d79408
GON <hutabasuzukiryu@live.jp>
2019-03-07 23:14:14 +0900

    change: ソリューションファイル追加

---
+++ BaseComponent/AltseedExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseComponent
+{
+    /// <summary>
+    /// Altseed拡張
+    /// </summary>
+    public static class AltseedExtension
+    {
+        /// <summary>
+        /// 座標が矩形領域に含まれているか
+        /// </summary>
+        /// <param name="rect">領域</param>
+        /// <param name="vector">座標</param>
+        /// <returns>真偽</returns>
+        public static bool Contains(this asd.RectF rect, asd.Vector2DF vector)
+        {
+            float cross = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                var temp = asd.Vector2DF.Cross(rect.Vertexes[i] - rect.Vertexes[i == 0 ? 3 : i - 1], vector - rect.Vertexes[i == 0 ? 3 : i - 1]);
+                if (temp == 0) return true;
+                if (Math.Sign(cross) == Math.Sign(temp) || cross == 0) cross = temp;
+                else return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ BaseComponent/IO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseComponent
+{
+    /// <summary>
+    /// ファイル入出力系
+    /// </summary>
+    public class IO
+    {
+        public static MemoryStream GetStream(string path)
+        {
+            if (path == null) throw new ArgumentNullException("path is null");
+            var temp = asd.Engine.File.CreateStaticFile(path);
+            if (temp == null) throw new FileNotFoundException(path + "が見つかりません");
+            var result = temp.Buffer;
+            return new MemoryStream(result);
+        }
+    }
+}
